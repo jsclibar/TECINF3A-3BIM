586c1d9991a82102a463aca7059aa54579eed0e8
“jsclibar” <jsclibar@gmail.com>
2025-08-15 08:29:26 -0300

    AT1 Funcoes

---
+++ FUNCOES/AT1.py
@@ -0,0 +1,11 @@
+'''
+Atividades Práticas:
+
+    1.	Crie uma função chamada boas_vindas() que exiba três mensagens diferentes.
+    2.	Crie uma função linha() que imprima uma linha com 30 traços (-) e use-a antes e depois de uma mensagem central.
+    3.	Monte uma função chamada menu() que mostre três opções no estilo:
+
+        1 - Ver saldo
+        2 - Sacar
+        3 - Sair
+'''
