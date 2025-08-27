be540ba9e3c984ef874995fb70d84b1fec172337
“jsclibar” <jsclibar@gmail.com>
2025-08-27 09:21:28 -0300

    Atualizações

---
+++ FUNCOES/AT3.py
@@ -0,0 +1,18 @@
+''''
+
+1. Crie uma função dobro(valor) que receba um número e retorne o dobro desse número.
+
+2. Crie uma função area_retangulo(base, altura) que calcule e retorne a área de um retângulo.
+
+3. Crie uma função calcula_media(n1, n2, n3) que calcule a média de três notas e retorne esse valor.
+   Depois, use o valor retornado para dizer se o aluno foi aprovado (média ≥ 7) ou reprovado.
+
+4. Crie uma função raiz_quadrada(numero) que retorne a raiz quadrada de um número. #Dica: Importe a biblioteca math e use a função sqrt.
+
+5. Crie uma função fatorial(n) que calcule o fatorial de um número inteiro (ex: 5! = 5x4x3x2x1).
+
+6. Crie uma função conversor_temperatura(celsius) que converta uma temperatura em graus Celsius para Fahrenheit e retorne o valor convertido.
+   Use a fórmula:
+   F = C x 1.8 + 32
+
+'''
---
+++ FUNCOES/EX3.py
@@ -0,0 +1,11 @@
+def soma(a, b):
+    return a + b
+
+def media(n1, n2):
+    return (n1 + n2) / 2
+
+
+resultado = soma(5, 3)
+print(f"Resultado: {resultado}")
+
+print(media(7, 8))
