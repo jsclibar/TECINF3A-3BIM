3e07e4af1148da43c3adac18d83c02ea4f333f68
“jsclibar” <jsclibar@gmail.com>
2025-08-27 09:07:29 -0300

    Atualizações

---
+++ FUNCOES/AT2.py
@@ -0,0 +1,44 @@
+''''
+
+Atividades Práticas:
+
+    1. Crie uma função que receba um nome como parâmetro e exiba uma mensagem personalizada entre duas linhas de traços.
+    Exemplo de saída:
+
+    ------------------------------
+    Olá, JOSÉ CARLOS!
+    ------------------------------
+
+    2. Crie uma função que receba a idade de uma pessoa e informe se ela é maior ou menor de idade.
+    Exemplo de saída:
+
+    Idade: 15 → Menor de idade  
+    Idade: 22 → Maior de idade
+
+    3. Crie uma função que receba uma mensagem e uma quantidade de vezes, e exiba a mensagem repetida conforme o número indicado.
+    Exemplo de chamada:
+
+    repetir_mensagem("Estudar é importante!", 3)
+
+    4. Crie uma função que receba um número inteiro e exiba a tabuada de multiplicação dele, de 1 até 10.
+    Exemplo de saída:
+
+    5 x 1 = 5  
+    5 x 2 = 10  
+    ...  
+    5 x 10 = 50
+
+    5. Crie uma função que classifique a pessoa de acordo com a idade:
+
+        •	Menor que 12 → Criança
+        •	De 12 a 17 → Adolescente
+        •	De 18 a 59 → Adulto
+        •	60 ou mais → Idoso
+
+    6. Crie uma função que receba o nome de uma pessoa e o turno (M = manhã, T = tarde, N = noite) e exiba uma saudação apropriada.
+    Exemplo:
+
+    mensagem_personalizada("Fernanda", "T")
+    # Saída: Boa tarde, Fernanda!
+    
+'''
---
+++ FUNCOES/EX2.py
@@ -0,0 +1,11 @@
+def saudacao(nome):
+    print(f"Olá, {nome}! Seja bem-vindo(a)!")
+
+def par_ou_impar(numero):
+    if numero % 2 == 0:
+        print(f"{numero} é par")
+    else:
+        print(f"{numero} é ímpar")
+
+#saudacao("Ana")
+#par_ou_impar(3)
