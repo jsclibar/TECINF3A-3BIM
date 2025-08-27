0a252100bdcdc1f1c2497d80504f89f2e3520a83
“jsclibar” <jsclibar@gmail.com>
2025-08-27 08:53:31 -0300

    Atualização

--- FUNCOES/AT1.py
+++ FUNCOES/AT1.py
@@ -9,3 +9,33 @@ Atividades Práticas:
         2 - Sacar
         3 - Sair
 '''
+
+def boas_vindas():
+    print("Olá, aluno(a)!")
+    print("Seja bem-vindo à aula de funções em Python.")
+    print("Vamos aprender a organizar melhor nossos programas!")
+
+def linha():
+    print("-" * 30)
+
+def menu():
+    print("MENU PRINCIPAL")
+    print("1 - Ver saldo")
+    print("2 - Sacar")
+    print("3 - Sair")
+
+#boas_vindas()
+
+# Uso da função com uma mensagem no meio
+#linha()
+#print("Curso de Programação em Python")
+#linha()
+
+#menu()
+
+while True:
+    linha()
+    menu()
+    opcao = input("Escolha uma opção (ou 'sair'): ")
+    if opcao.lower() == 'sair':
+        break
